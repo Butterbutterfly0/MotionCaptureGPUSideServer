2e62409e68aeae53b18e83325d19ad69172c4726
Butterbutterfly0 <z1125462110@163.com>
2026-09-11 21:59:49 +0800

    upload and get result video function  complete

--- .ipynb_checkpoints/server-checkpoint.py
+++
@@ -1,4 +0,0 @@
-import WHAM
-from WHAM import wham_api
-
-print(dir(WHAM))
--- server.py
+++ server.py
@@ -4,12 +4,17 @@
 # print(dir(WHAM))i
 from fastapi import FastAPI, UploadFile,File
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import BackgroundTasks
+from fastapi.responses import FileResponse
 import aiofiles
 import os
 import uuid
+import shutil
 
-UPLOAD_DIR = "tmp"        # 你现在的设置
-os.makedirs(UPLOAD_DIR, exist_ok=True)   # ← 加这一行，不存在就建
+UPLOAD_DIR = "tmp/uploads"
+OUTPUT_DIR = "tmp/outputs"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 app = FastAPI()
 
@@ -20,13 +25,33 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+tasks: dict[str, dict] = {}
+
+
+def fake_process(task_id: str, video_path: str):
+    tasks[task_id]["status"] = "running"
+    total = 5
+    for i in range(total):
+        import time
+        time.sleep(0.1)              # 假装处理 1 秒
+        tasks[task_id]["progress"] = int((i + 1) / total * 100)
+
+    output_path = os.path.join(OUTPUT_DIR,f'{task_id}.mp4')
+    shutil.copy(video_path, output_path)
+
+    tasks[task_id]["status"] = "done"
+    tasks[task_id]["result"] = output_path
+    tasks[task_id]["progress"] = 100
+
 @app.post("/api/upload")
-async def upload(file:UploadFile = File(...)):
+async def upload(file:UploadFile = File(...), bg: BackgroundTasks = None):
     task_id = str(uuid.uuid4())
     save_path = os.getcwd() +  f"/tmp/{file.filename}"
     async with aiofiles.open(save_path,"wb") as out:
         while chunk := await file.read(1024*1024):
             await out.write(chunk)
+    tasks[task_id] = {"status": "pending", "progress": 0, "result": None}
+    bg.add_task(fake_process, task_id, save_path)
     return {"task_id":task_id,
             "filename":file.filename,
              "path":save_path}
@@ -34,3 +59,24 @@ async def upload(file:UploadFile = File(...)):
 @app.get("/api/ping")
 async def ping():
     return {"ok":True}
+
+@app.get("/api/task/{task_id}")
+async def get_task(task_id: str):
+    if task_id not in tasks:
+        raise HTTPException(404, "Task Not Found")
+    t = tasks[task_id]
+    return {
+        "status":t["status"],
+        "progress":t["progress"],
+        "result":t["result"]
+    }
+
+
+@app.get("/api/result/{task_id}")
+async def get_result(task_id: str):
+    if task_id not in tasks:
+        raise HTTPException(404, "Task Not Found")
+    t = tasks[task_id]
+    if t["status"] != "done" or not t["result"]:
+        raise HTTPException(400, "Result not ready")
+    return FileResponse(t["result"], media_type="video/mp4")
